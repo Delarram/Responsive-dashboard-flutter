f593b57f9cacd0c71c12a5adcc2413df0c5cfc96
Delarram <tunyanaing960@gmail.com>
2023-07-09 00:43:02 +0630

    feat/dl

---
+++ lib/screens/dashboard_screen/dashboard_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashBoardScreen extends StatelessWidget {
+  const DashBoardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: SingleChildScrollView(
+      child: Column(
+        children: [
+        ],
+      ),
+    ));
+  }
+}
