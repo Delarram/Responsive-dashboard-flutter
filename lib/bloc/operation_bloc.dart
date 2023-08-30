d474cf12aebeff5edd25fc5dd3dba618bc6bcce6
Delarram <tunyanaing960@gmail.com>
2023-08-30 14:14:42 +0630

    added bloc

---
+++ lib/bloc/operation_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+import '../model/contact_model.dart';
+
+
+class OperationBloc extends ChangeNotifier{
+List<Contact> contacts = [];
+
+void addContact(Contact contact){
+  contacts.add(contact);
+  notifyListeners();
+}
+
+void updateContact (int index, Contact contact){
+  contacts[index] = contact;
+  notifyListeners();
+}
+
+void deleteContact (int index){
+  contacts.removeAt(index);
+  notifyListeners();
+}
+
+}
